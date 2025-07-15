662044fb6762dd25bca350a9444f2ac361cb528e
Valberton <valberton77@gmail.com>
2025-07-15 11:39:39 -0300

    send email methods added

---
+++ src/main/java/com/proj/notification/infrastructure/exception/EmailException.java
@@ -0,0 +1,12 @@
+package com.proj.notification.infrastructure.exception;
+
+public class EmailException extends RuntimeException {
+
+    public EmailException(String message) {
+        super(message);
+    }
+
+    public EmailException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
